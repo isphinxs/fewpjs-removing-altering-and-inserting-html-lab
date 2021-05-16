e6e13f4ee200d958f693c40eff3bc0608a1f0e84
Samantha Ostrowski <isphinxs@hotmail.com>
2021-05-16 15:19:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main = document.getElementById("main");
+main.remove();
+let element = document.querySelector("body");
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Sam is the champion";
+element.appendChild(newHeader);
